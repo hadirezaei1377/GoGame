4725b57d8ca80cba60cc18aefc4a48a7a551e87e
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-12-26 22:42:22 +0330

    prerequisites

---
+++ game/game.go
@@ -0,0 +1,23 @@
+package game
+
+// the types that the player can have
+// Dog
+type Dog struct {
+	Name  string
+	speed int
+	x     int
+}
+
+// Cat
+type Cat struct {
+	Name  string
+	speed int
+	x     int
+}
+
+// Owl
+type Owl struct {
+	Name  string
+	speed int
+	x     int
+}
