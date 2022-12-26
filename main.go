309667e5935fba5dd2a51cc88b5cb73c999f28c9
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-12-26 22:37:16 +0330

    todo list

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	// make players
+	// make the game(an object named game which has a series of game)
+	// join players to the game
+	// loop til there is a winner
+	/* in loop
+	// move the players
+	// check is there any winner?
+	// print the final statement and print the winner
+	*/
+
+}
